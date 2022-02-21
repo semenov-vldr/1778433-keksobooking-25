6b0cd461721b58806609a92fc3e159f17bf50551
Vladimir <vova2066@yandex.ru>
2022-02-21 11:32:13 +0300

    исправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
-let getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   if (min >= 0 & max >= 0 & max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor (Math.random() * (max - min + 1) + min);
-  } return "Неверный диапазон!";
+  } return 'Неверный диапазон!';
 };
 
-console.log(getRandomInt(1, 10));
+getRandomInt(1, 10);
 
 
-
-let getRandomFloat = (min, max, numsign) => {
+const getRandomFloat = (min, max, numsign) => {
   if (min >= 0 & max >= 0 & max > min) {
-    let result = Math.random() * (max-min) + min;
+    const result = Math.random() * (max-min) + min;
     return result.toFixed(numsign);
-  } return "Неверный диапазон!";
+  } return 'Неверный диапазон!';
 };
 
-console.log(getRandomFloat(3.5, 3.6, 2));
+getRandomFloat(3.5, 3.6, 2);
 

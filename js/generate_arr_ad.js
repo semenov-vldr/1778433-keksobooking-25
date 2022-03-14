6caa956f8106e0cbfdac27ebce521e63726244ca
Vladimir <vova2066@yandex.ru>
2022-03-14 21:40:20 +0300

    переделал ф-цию generateArrAd

--- js/generate_arr_ad.js
+++ js/generate_arr_ad.js
@@ -94,31 +94,4 @@ const descriptionAdd = () => ({
 
 const generateArrAd = new Array(1).fill(null).map(() => descriptionAdd());
 
-console.log(generateArrAd[0].generateInfoHouse.title); // нужно упростить способ обращения
-
-//---------------------------------------------------------------
-
-const mapCanvas = document.querySelector('#map-canvas');
-
-// Шаблон для клонирования
-const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
-
-const generatePopup = generateArrAd;
-
-generatePopup.forEach((offer) => {
-  const popupItem = cardTemplate.cloneNode(true);
-
-
-  //popupItem.querySelector('.popup__title').textContent = offer.title;
-  //popupItem.querySelector('.popup__text--address').textContent = 'тут будет адрес';
-  // popupItem.querySelector('.popup__text--price').textContent = offer.price;
-  // popupItem.querySelector('.popup__type').textContent = offer.type;
-  // popupItem.querySelector('..popup__text--capacity').textContent = offer.rooms + 'комнаты для ' + offer.guests + 'гостей';
-  // popupItem.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ' , выезд до ' + offer.checkout;
-  // popupItem.querySelector('.popup__features').textContent = offer.features;
-  // popupItem.querySelector('.popup__description').textContent = offer.description;
-  // popupItem.querySelector('.popup__photos').src = offer.photos;
-  // popupItem.querySelector('.popup__avatar').src = author.avatar;
-
-  mapCanvas.appendChild(popupItem);
-});
+console.log(generateArrAd);
--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,1 @@
-import {generateArrAd} from './main.js';
-
-const page = document.querySelector('main');
-
-const mapCanvas = document.querySelector('#map-canvas');
-
-// Шаблон для клонирования
-const cardTemplate = document.querySelector('#card').сontent.querySelector('.popup');
-
-
-const popupItem = cardTemplate.cloneNode(true);
-mapCanvas.appendChild(popupItem);
 

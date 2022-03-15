b248657ff8f886c960b3c0adfb8cff3b9494b0ad
Vladimir <vova2066@yandex.ru>
2022-03-15 15:32:29 +0300

    подготовил ф-цию для заполнения данными шаблона

--- js/generate_arr_ad.js
+++ js/generate_arr_ad.js
@@ -1,4 +1,4 @@
-import {getRandomInt, getRandomFloat} from './get_random_value.js';
+import {getRandomInt, getRandomFloat, getRandomArrayElement, getRandomArrayLength} from './get_random_value.js';
 
 
 const generateAvatar = () => {
@@ -7,22 +7,23 @@ const generateAvatar = () => {
   return avatar;
 };
 
-
 const generateInfoHouse = () => {
-
-  const place = {
+  const address = {
     lat: getRandomFloat(35.65, 35.7, 5),
     lng: getRandomFloat(139.7, 139.8, 5),
   };
 
+  const price =  getRandomInt(1000, 10000);
 
-  const housingPrice =  getRandomInt(10, 100);
-
-  const randomHousingType = () =>  {
-    const housingTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-    return housingTypes[getRandomInt(0, housingTypes.length - 1)];
+  const TYPES = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalow: 'Бунгало',
+    hotel: 'Отель',
   };
 
+  const type = getRandomArrayElement(Object.values(TYPES));
 
   const rooms = getRandomInt(1, 10);
 
@@ -30,68 +31,50 @@ const generateInfoHouse = () => {
 
   const checkinTimes = ['12:00', '13:00', '14:00'];
   const getCheckinTime = () => checkinTimes[getRandomInt(0, checkinTimes.length - 1)];
-
   const getCheckoutTime = () => checkinTimes[getRandomInt(0, checkinTimes.length - 1)];
 
-  const getRandomFeatures = () => {
+  const features = ['wifi', 'dishwashar', 'parking', 'washer', 'elevator', 'conditioner'];
 
-    const featuresList = ['wifi', 'dishwashar', 'parking', 'washer', 'elevator', 'conditioner'];
-
-    const features = new Set();
-    const featuresLength = getRandomInt(1, featuresList.length);
-
-    while (features.size < featuresLength) {
-      const randomFeature = featuresList[getRandomInt(0, featuresList.length-1)];
-      features.add(randomFeature);
-    }
-    return [...features];
-  };
-
-  const getRandomPhotos = () => {
-    const photosArr = [
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-    ];
-
-    return photosArr.slice(0, getRandomInt(1, photosArr.length));
-  };
+  const photosArr = [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  ];
 
   const offer = {
-    title: 'Заголовок предложения', // заменить
-    address: place,
-    housingPrice,
-    type: randomHousingType(),
+    title: 'Заголовок предложения',
+    address,
+    price,
+    type,
     rooms,
     guests,
     checkin: getCheckinTime(checkinTimes),
     checkout: getCheckoutTime(checkinTimes),
-    features: getRandomFeatures(),
-    description: 'Описание помещения', // заменить
-    photos: getRandomPhotos(),
+    features: getRandomArrayLength(features),
+    description,
+    photos: getRandomArrayLength(photosArr),
   };
 
+
   switch(offer.type) {
-    case 'palace': offer.description = 'Дворец для настоящего королевского отдыха';
+    case 'Дворец': offer.description = 'Дворец для настоящего королевского отдыха';
       break;
-    case 'flat': offer.description = 'Светлая и просторная квартира с видом на море';
+    case 'Квартира': offer.description = 'Светлая и просторная квартира с видом на море';
       break;
-    case 'house': offer.description = 'Большой двухэтажный дом в экологически чистом месте';
+    case 'Дом': offer.description = 'Большой двухэтажный дом в экологически чистом месте';
       break;
-    case 'bungalow': offer.description = 'Калифорнийское бунгало с колоннами и покатой крышей';
+    case 'Бунгало': offer.description = 'Калифорнийское бунгало с колоннами и покатой крышей';
       break;
-    case 'hotel': offer.description = 'Лучший отель в городе';
+    case 'Отель': offer.description = 'Лучший отель в городе';
       break;
   }
 
   return offer;
 };
 
-const descriptionAdd = () => ({
-  generateAvatar: generateAvatar(),
-  generateInfoHouse: generateInfoHouse(),
-});
-
-const generateArrAd = new Array(1).fill(null).map(() => descriptionAdd());
+const generateArrAd = new Array(1).fill(null).map(() => ({
+  avatar: generateAvatar(),
+  ...generateInfoHouse(),
+}));
 
 export {generateArrAd};
--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,50 @@
-
 import {generateArrAd} from './generate_arr_ad.js';
-
-
-const mapCanvas = document.querySelector('#map-canvas');
 
 // Шаблон для клонирования
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+const popupItem = cardTemplate.cloneNode(true);
+const mapCanvas = document.querySelector('#map-canvas');
+const featureListElement = popupItem.querySelector('.popup__features');
+const photoListElement = document.querySelector('.popup__photos');
+const photoListFragment = document.createDocumentFragment();
+const descriptionElement = popupItem.querySelector('.popup__description');
+
 
 const generatePopup = generateArrAd;
 
 generatePopup.forEach((offer) => {
-  const popupItem = cardTemplate.cloneNode(true);
-
-
-  //popupItem.querySelector('.popup__title').textContent = offer.title;
-  //popupItem.querySelector('.popup__text--address').textContent = 'тут будет адрес';
-  // popupItem.querySelector('.popup__text--price').textContent = offer.price;
-  // popupItem.querySelector('.popup__type').textContent = offer.type;
-  // popupItem.querySelector('..popup__text--capacity').textContent = offer.rooms + 'комнаты для ' + offer.guests + 'гостей';
-  // popupItem.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ' , выезд до ' + offer.checkout;
-  // popupItem.querySelector('.popup__features').textContent = offer.features;
-  // popupItem.querySelector('.popup__description').textContent = offer.description;
-  // popupItem.querySelector('.popup__photos').src = offer.photos;
-  // popupItem.querySelector('.popup__avatar').src = author.avatar;
-
+  popupItem.querySelector('.popup__title').textContent = offer.title;
+  popupItem.querySelector('.popup__text--address').textContent = offer.address;
+  popupItem.querySelector('.popup__text--price').textContent = `${offer.price  } ₽/ночь`;
+  popupItem.querySelector('.popup__type').textContent = offer.type;
+  popupItem.querySelector('.popup__text--capacity').textContent = `${offer.rooms  } комнаты для ${  offer.guests  } гостей`;
+  popupItem.querySelector('.popup__text--time').textContent = `Заезд после ${  offer.checkin  }, выезд до ${  offer.checkout}`;
+  popupItem.querySelector('.popup__features').textContent = offer.features;
+  descriptionElement.textContent = offer.description;
+  //popupItem.querySelector('.popup__photo').src = offer.photos;
+  popupItem.querySelector('.popup__avatar').src = offer.avatar;
   mapCanvas.appendChild(popupItem);
+
+  // Добавление дополнительных тегов img для фото
+  offer.photos.forEach((photo) => {
+    const photoItem = document.querySelector('.popup__photo').cloneNode(true);
+    photoItem.src = photo;
+    photoListFragment.appendChild(photoItem);
+  });
+  photoListElement.textContent = '';
+  photoListElement.appendChild(photoListFragment);
+
+  // Скрытие блока, если нет данных
+  const checkDataAvailable = (content, element) => {
+    if (!content.length) {
+      element.classList.add('hidden');
+    }
+  };
+
+  checkDataAvailable(offer.features, featureListElement);
+  checkDataAvailable(offer.photos, photoListElement);
+  checkDataAvailable(offer.description, descriptionElement);
+
 });
+
+
